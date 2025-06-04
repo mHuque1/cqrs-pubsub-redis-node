63e20885d136d40a7c879bc136811c54b591eeb1
Mathias Huque <mh284934@fi365.ort.edu.uy>
2025-06-04 17:19:58 -0300

    aaaaa

--- Server-B/src/index.ts
+++ Server-B/src/index.ts
@@ -3,12 +3,14 @@ import 'dotenv/config';
 import dataRoutes from './routes/data.routes';
 import connectDB from './services/db.service';
 import startQueueConsumer from './services/queue.service';
+import userRoutes from 'routes/user.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/all', dataRoutes);
+app.use('/users', userRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
